21965ffe1d1e00656e42e3e178282140a026de77
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-09 09:22:43 -0300

    Adiciona rotas React

---
+++ frontend/src/main.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { GrupoParceirosPage } from './pages/GrupoParceirosPage';
+import { GrupoProdutosPage } from './pages/GrupoProdutosPage';
+import { ParceirosPage } from './pages/ParceirosPage';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Navigate to="/parceiros" replace /> },
+      { path: 'parceiros', element: <ParceirosPage /> },
+      { path: 'grupo-parceiros', element: <GrupoParceirosPage /> },
+      { path: 'grupo-produtos', element: <GrupoProdutosPage /> }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
